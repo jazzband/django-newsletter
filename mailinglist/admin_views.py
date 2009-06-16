a3bb5359cfafb8ef6e573f828590d6764e7e1526
Mathijs de Bruin <mathijs@visualspace.nl>
2009-06-16 17:52:38 +0200

    Previews moved to newforms-admin. Getting ready to pull out all URLs.

--- mailinglist/admin_views.py
+++ mailinglist/admin_views.py
@@ -19,55 +19,6 @@ def json_subscribers(request, myid):
     return HttpResponse(json, mimetype='application/json')
 
 @staff_member_required
-def message_preview(request, myid):
-    message = get_object_or_404(Message, id=myid)
-
-    return render_to_response(
-        "admin/mailinglist/message/preview.html",
-        { 'message' : message },
-        RequestContext(request, {}),
-    )
-
-@staff_member_required
-def message_submit(request, myid):
-    message = get_object_or_404(Message, id=myid)
-    
-    submission = Submission.from_message(message)
-    
-    return HttpResponseRedirect('../../../submission/%s/' % submission.id)
-
-@staff_member_required
-def html_preview(request, myid):
-    message = get_object_or_404(Message, id=myid)
-
-    (subject_template, text_template, html_template) = EmailTemplate.get_templates('message', message.newsletter)
-
-    if not html_template:
-        # If no HTML available, return an empty bogus HTML page.
-        #return HttpResponse('<html><head><title></title></head><body></body></html>')
-        raise Http404
-        
-    c = Context({'message' : message, 
-                 'site' : Site.objects.get_current(),
-                 'newsletter' : message.newsletter,
-                 'date' : datetime.now()})
-                 
-    return HttpResponse(html_template.render(c))
-
-@staff_member_required
-def text_preview(request, myid):
-    message = get_object_or_404(Message, id=myid)
-
-    (subject_template, text_template, html_template) = EmailTemplate.get_templates('message', message.newsletter)
-
-    c = Context({'message' : message, 
-                 'site' : Site.objects.get_current(),
-                 'newsletter' : message.newsletter,
-                 'date' : datetime.now()})
-                 
-    return HttpResponse(text_template.render(c), mimetype='text/plain')
-
-@staff_member_required
 def submit(request, myid):
     submission = get_object_or_404(Submission, id=myid)
     
