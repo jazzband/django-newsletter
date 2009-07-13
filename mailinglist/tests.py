178c0e67562ab879b3b002e8870b9711cc3ea684
Mathijs de Bruin <mathijs@visualspace.nl>
2009-07-13 19:02:34 +0200

    - Removed Site from tests, for now. - Added update tests. - Fixed WebTestCase.

--- mailinglist/tests.py
+++ mailinglist/tests.py
@@ -21,7 +21,7 @@ class WebTestCase(TestCase):
             instance = response.context[variable]
             self.assert_(instance)
         except KeyError:
-            self.fail('Asserted variable %s not in response context.')
+            self.fail('Asserted variable %s not in response context.' % variable)
         
         if instance_of:
             self.assert_(isinstance(instance, instance_of))
@@ -93,7 +93,6 @@ class WebSubscribeTestCase(WebTestCase, MailTestCase):
         self.assertNotContains(r, 'input id="id_email" type="text" name="email"')
         
         self.assertInContext(r, 'newsletter', Newsletter, self.n)
-        self.assertInContext(r, 'site', Site, self.site)        
         self.assertInContext(r, 'form', SubscribeRequestForm)
         
         subscription = getattr(r.context['form'], 'instance', None)
@@ -119,12 +118,67 @@ class WebSubscribeTestCase(WebTestCase, MailTestCase):
         activate_url = subscription.subscribe_activate_url()
         self.assert_(activate_url)
 
-        r = self.client.post(activate_url, {'name':'Test Name', 'email':'test@email.com', 'user_activation_code':subscription.activation_code})
+        r = self.client.get(activate_url)
+        self.assertInContext(r, 'form', UpdateForm)
+        self.assertContains(r, subscription.activation_code)
         
+        r = self.client.post(activate_url, {'name':'Test Name', 'email':'test@email.com', 'user_activation_code':subscription.activation_code})        
         self.assertInContext(r, 'form', UpdateForm)
         
         subscription = getattr(r.context['form'], 'instance', None)
         self.assert_(subscription)
         self.assert_(subscription.activated)
         self.assertFalse(subscription.unsubscribed)
+    
+    def test_update_request_view(self):
+        """ Test the update request form. """
+        r = self.client.get(self.update_url)
+        
+        self.assertContains(r, self.n.title, status_code=200)
+        self.assertContains(r, 'input id="id_email" type="text" name="email"')
+        
+        self.assertEqual(r.context['newsletter'], self.n)
+    
+    def test_update_request_post(self):
+        """ Post the update request form. """
+        subscription = Subscription(newsletter=self.n, name='Test Name', email='test@email.com', activated=True)
+        subscription.save()
+        
+        r = self.client.post(self.update_url, {'email':'test@email.com'})
+        
+        self.assertContains(r, self.n.title, status_code=200)
+        self.assertNotContains(r, 'input id="id_email" type="text" name="email"')
+        
+        self.assertInContext(r, 'newsletter', Newsletter, self.n)
+        self.assertInContext(r, 'form', UpdateRequestForm)
+        
+        self.assertEqual(subscription, r.context['form'].instance)
+        
+        """ Check the subscription email. """
+        self.assertEquals(len(mail.outbox), 1)
+                
+        activate_url = subscription.update_activate_url()
+        full_activate_url = 'http://%s%s' % (self.site.domain, activate_url)
+        
+        self.assertEmailContains(full_activate_url)
+    
+    def test_update_request_activate(self):
+        """ Update a request. """
+        subscription = Subscription(newsletter=self.n, name='Test Name', email='test@email.com')
+        subscription.save()
 
+        activate_url = subscription.update_activate_url()
+        
+        r = self.client.get(activate_url)
+        self.assertInContext(r, 'form', UpdateForm)
+        self.assertContains(r, subscription.activation_code)
+        
+        testname2 = 'Test Name2'
+        testemail2 = 'test2@email.com'
+        r = self.client.post(activate_url, {'name':testname2, 'email':testemail2, 'user_activation_code':subscription.activation_code})        
+        self.assertInContext(r, 'form', UpdateForm)
+        
+        subscription = getattr(r.context['form'], 'instance', None)
+        self.assert_(subscription)
+        self.assertEqual(subscription.name, testname2)
+        self.assertEqual(subscription.email, testemail2)
