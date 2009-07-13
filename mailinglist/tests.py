ae3715ec124fb98de723ea9bed30899f18eb9f62
Mathijs de Bruin <mathijs@visualspace.nl>
2009-07-13 19:11:29 +0200

    Added basic tests for unsubscribing as well.

--- mailinglist/tests.py
+++ mailinglist/tests.py
@@ -63,16 +63,15 @@ class WebSubscribeTestCase(WebTestCase, MailTestCase):
         self.unsubscribe_url = reverse('mailinglist_newsletter_unsubscribe_request', 
                                        kwargs={'newsletter_slug' : self.n.slug })
                                        
-        self.update_url = reverse('mailinglist_newsletter_update_request', 
+        self.unsubscribe_url = reverse('mailinglist_newsletter_unsubscribe_request', 
                                   kwargs={'newsletter_slug' : self.n.slug })
         
         super(WebSubscribeTestCase, self).setUp()
-
     
     def test_urls(self):
         self.assert_(len(self.subscribe_url))
         self.assert_(len(self.unsubscribe_url))
-        self.assert_(len(self.update_url))
+        self.assert_(len(self.unsubscribe_url))
     
     def test_subscribe_request_view(self):
         """ Test the subscription form. """
@@ -130,21 +129,21 @@ class WebSubscribeTestCase(WebTestCase, MailTestCase):
         self.assert_(subscription.activated)
         self.assertFalse(subscription.unsubscribed)
     
-    def test_update_request_view(self):
-        """ Test the update request form. """
-        r = self.client.get(self.update_url)
+    def test_unsubscribe_request_view(self):
+        """ Test the unsubscribe request form. """
+        r = self.client.get(self.unsubscribe_url)
         
         self.assertContains(r, self.n.title, status_code=200)
         self.assertContains(r, 'input id="id_email" type="text" name="email"')
         
         self.assertEqual(r.context['newsletter'], self.n)
     
-    def test_update_request_post(self):
-        """ Post the update request form. """
+    def test_unsubscribe_request_post(self):
+        """ Post the unsubscribe request form. """
         subscription = Subscription(newsletter=self.n, name='Test Name', email='test@email.com', activated=True)
         subscription.save()
         
-        r = self.client.post(self.update_url, {'email':'test@email.com'})
+        r = self.client.post(self.unsubscribe_url, {'email':'test@email.com'})
         
         self.assertContains(r, self.n.title, status_code=200)
         self.assertNotContains(r, 'input id="id_email" type="text" name="email"')
@@ -157,17 +156,17 @@ class WebSubscribeTestCase(WebTestCase, MailTestCase):
         """ Check the subscription email. """
         self.assertEquals(len(mail.outbox), 1)
                 
-        activate_url = subscription.update_activate_url()
+        activate_url = subscription.unsubscribe_activate_url()
         full_activate_url = 'http://%s%s' % (self.site.domain, activate_url)
         
         self.assertEmailContains(full_activate_url)
     
-    def test_update_request_activate(self):
+    def test_unsubscribe_request_activate(self):
         """ Update a request. """
         subscription = Subscription(newsletter=self.n, name='Test Name', email='test@email.com')
         subscription.save()
-
-        activate_url = subscription.update_activate_url()
+        
+        activate_url = subscription.unsubscribe_activate_url()
         
         r = self.client.get(activate_url)
         self.assertInContext(r, 'form', UpdateForm)
@@ -182,3 +181,55 @@ class WebSubscribeTestCase(WebTestCase, MailTestCase):
         self.assert_(subscription)
         self.assertEqual(subscription.name, testname2)
         self.assertEqual(subscription.email, testemail2)
+    
+    def test_unsubscribe_request_view(self):
+        """ Test the unsubscribe request form. """
+        r = self.client.get(self.unsubscribe_url)
+        
+        self.assertContains(r, self.n.title, status_code=200)
+        self.assertContains(r, 'input id="id_email" type="text" name="email"')
+        
+        self.assertEqual(r.context['newsletter'], self.n)
+    
+    def test_unsubscribe_request_post(self):
+        """ Post the unsubscribe request form. """
+        subscription = Subscription(newsletter=self.n, name='Test Name', email='test@email.com', activated=True)
+        subscription.save()
+        
+        r = self.client.post(self.unsubscribe_url, {'email':'test@email.com'})
+        
+        self.assertContains(r, self.n.title, status_code=200)
+        self.assertNotContains(r, 'input id="id_email" type="text" name="email"')
+        
+        self.assertInContext(r, 'newsletter', Newsletter, self.n)
+        self.assertInContext(r, 'form', UpdateRequestForm)
+        
+        self.assertEqual(subscription, r.context['form'].instance)
+        
+        """ Check the subscription email. """
+        self.assertEquals(len(mail.outbox), 1)
+                
+        activate_url = subscription.unsubscribe_activate_url()
+        full_activate_url = 'http://%s%s' % (self.site.domain, activate_url)
+        
+        self.assertEmailContains(full_activate_url)
+    
+    def test_unsubscribe_request_activate(self):
+        """ Update a request. """
+        subscription = Subscription(newsletter=self.n, name='Test Name', email='test@email.com')
+        subscription.save()
+        
+        activate_url = subscription.unsubscribe_activate_url()
+        
+        r = self.client.get(activate_url)
+        self.assertInContext(r, 'form', UpdateForm)
+        self.assertContains(r, subscription.activation_code)
+        
+        testname2 = 'Test Name2'
+        testemail2 = 'test2@email.com'
+        r = self.client.post(activate_url, {'name':testname2, 'email':testemail2, 'user_activation_code':subscription.activation_code})        
+        self.assertInContext(r, 'form', UpdateForm)
+        
+        subscription = getattr(r.context['form'], 'instance', None)
+        self.assert_(subscription)
+        self.assert_(subscription.unsubscribed)
