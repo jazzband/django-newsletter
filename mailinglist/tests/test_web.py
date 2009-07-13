6833b8f40e293768c389881c9f979df0aed830d5
Mathijs de Bruin <mathijs@visualspace.nl>
2009-07-13 20:04:59 +0200

    Moved tests to allow for separate files.

--- mailinglist/tests/test_web.py
+++ mailinglist/tests/test_web.py
@@ -1,53 +1,12 @@
-import logging
-
 from django.core import mail
 
 from django.core.urlresolvers import reverse
 
-from django.test import TestCase
-from django.test.client import Client
+from mailinglist.models import *
+from mailinglist.forms import *
 
-from models import *
-from forms import *
+from utils import *
 
-class WebTestCase(TestCase):
-    def setUp(self):
-        self.site = Site.objects.get_current()
-        
-        super(WebTestCase, self).setUp()
-
-    def assertInContext(self, response, variable, instance_of=None, value=None):
-        try:
-            instance = response.context[variable]
-            self.assert_(instance)
-        except KeyError:
-            self.fail('Asserted variable %s not in response context.' % variable)
-        
-        if instance_of:
-            self.assert_(isinstance(instance, instance_of))
-        
-        if value:
-            self.assertEqual(instance, value)
-
-class MailTestCase(TestCase):    
-    def get_email_list(self, email):
-        if email:
-            return (email,)
-        else:
-            return mail.outbox
-    
-    def assertEmailContains(self, value, email=None):
-        for my_email in self.get_email_list(email):
-            self.assert_((value in my_email.subject) or (value in my_email.body), 'Email does not contain "%s".' % value)
-    
-    def assertEmailBodyContains(self, value, email=None):
-        for my_email in self.get_email_list(email):
-            self.assert_(value in my_email.body, 'Email body does not contain "%s".' % value)
-            
-    def assertEmailSubjectContains(self, value, email=None):
-        for my_email in self.get_email_list(email):
-            self.assert_(value in my_email.subject, 'Email subject does not contain "%s".' % value)
-    
 class WebSubscribeTestCase(WebTestCase, MailTestCase):
     def setUp(self):
         self.n = Newsletter(title='Test newsletter',
