ac077bad48c4b15bfe3e7d2af1535805ad665e81
Mathijs de Bruin <drbob@dokterbob.net>
2010-06-30 16:08:05 +0200

    Extendible admin to different file

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.defaults import patterns, url
 
 from django.contrib import admin
-from django.contrib.admin.util import unquote, force_unicode
+from django.contrib.admin.util import force_unicode
 from django.contrib.sites.models import Site
 
 from django.core import serializers
@@ -36,34 +36,7 @@ except ImportError:
 from models import EmailTemplate, Newsletter, Subscription, Article, Message, Submission
 
 from admin_forms import *
-
-class ExtendibleModelAdminMixin(object):
-    def _getobj(self, request, object_id):
-            opts = self.model._meta
-            app_label = opts.app_label
-    
-            try:
-                obj = self.queryset(request).get(pk=unquote(object_id))
-            except self.model.DoesNotExist:
-                # Don't raise Http404 just yet, because we haven't checked
-                # permissions yet. We don't want an unauthenticated user to be able
-                # to determine whether a given object exists.
-                obj = None
-    
-            if obj is None:
-                raise Http404(_('%(name)s object with primary key %(key)r does not exist.') % {'name': force_unicode(opts.verbose_name), 'key': escape(object_id)})
-
-            return obj
-    
-    def _wrap(self, view):
-        def wrapper(*args, **kwargs):
-            return self.admin_site.admin_view(view)(*args, **kwargs)
-        return update_wrapper(wrapper, view)
-    
-    def _view_name(self, name):
-        info = self.model._meta.app_label, self.model._meta.module_name, name
-
-        return '%s_%s_%s' % info
+from admin_utils import *
 
 class NewsletterAdmin(admin.ModelAdmin):
     list_display = ('title', 'admin_subscriptions', 'admin_messages', 'admin_submissions')
