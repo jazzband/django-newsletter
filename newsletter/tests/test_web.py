60b7e2712710633d3ebc96ae00fbc0b6c3ba4c67
Mathijs de Bruin <drbob@dokterbob.net>
2010-07-26 17:37:47 +0200

    Cleanup and removal of duplicate code

--- newsletter/tests/test_web.py
+++ newsletter/tests/test_web.py
@@ -376,22 +376,13 @@ class AnonymousSubscribeTestCase(WebSubscribeTestCase, ComparingTestCase):
 
         dt = (subscription.subscribe_date - subscription.create_date).seconds
         self.assertBetween(dt, WAIT_TIME, WAIT_TIME+1)
-
-    def test_unsubscribe_request_view(self):
-        """ Test the unsubscribe request form. """
-        r = self.client.get(self.unsubscribe_url)
-
-        self.assertContains(r, self.n.title, status_code=200)
-        self.assertContains(r, 'input id="id_email_field" type="text" name="email"')
-
-        self.assertEqual(r.context['newsletter'], self.n)
-
+        
     def test_unsubscribe_request_post(self):
         """ Post the unsubscribe request form. """
         subscription = Subscription(newsletter=self.n,
                                     name='Test Name',
                                     email='test@email.com',
-                                    activated=True)
+                                    subscribed=True)
         subscription.save()
 
         r = self.client.post(self.unsubscribe_url, {'email_field': 'test@email.com'})
@@ -412,32 +403,6 @@ class AnonymousSubscribeTestCase(WebSubscribeTestCase, ComparingTestCase):
 
         self.assertEmailContains(full_activate_url)
 
-    def test_unsubscribe_request_activate(self):
-        """ Update a request. """
-        subscription = Subscription(newsletter=self.n,
-                                    name='Test Name',
-                                    email='test@email.com')
-        subscription.save()
-
-        activate_url = subscription.unsubscribe_activate_url()
-
-        r = self.client.get(activate_url)
-        self.assertInContext(r, 'form', UpdateForm)
-        self.assertContains(r, subscription.activation_code)
-
-        testname2 = 'Test Name2'
-        testemail2 = 'test2@email.com'
-        r = self.client.post(activate_url,
-                {'name_field': testname2,
-                 'email_field': testemail2,
-                 'user_activation_code': subscription.activation_code})
-        self.assertInContext(r, 'form', UpdateForm)
-
-        subscription = getattr(r.context['form'], 'instance', None)
-        self.assert_(subscription)
-        self.assertEqual(subscription.name, testname2)
-        self.assertEqual(subscription.email, testemail2)
-
     def test_unsubscribe_request_view(self):
         """ Test the unsubscribe request form. """
         r = self.client.get(self.unsubscribe_url)
@@ -446,33 +411,6 @@ class AnonymousSubscribeTestCase(WebSubscribeTestCase, ComparingTestCase):
         self.assertContains(r, 'input id="id_email_field" type="text" name="email_field"')
 
         self.assertEqual(r.context['newsletter'], self.n)
-
-    def test_unsubscribe_request_post(self):
-        """ Post the unsubscribe request form. """
-        subscription = Subscription(newsletter=self.n,
-                                    name='Test Name',
-                                    email='test@email.com',
-                                    subscribed=True)
-        subscription.save()
-
-        r = self.client.post(self.unsubscribe_url,
-                             {'email_field': 'test@email.com'})
-
-        self.assertContains(r, self.n.title, status_code=200)
-        self.assertNotContains(r, 'input id="id_email_field" type="text" name="email"')
-
-        self.assertInContext(r, 'newsletter', Newsletter, self.n)
-        self.assertInContext(r, 'form', UpdateRequestForm)
-
-        self.assertEqual(subscription, r.context['form'].instance)
-
-        """ Check the subscription email. """
-        self.assertEquals(len(mail.outbox), 1)
-
-        activate_url = subscription.unsubscribe_activate_url()
-        full_activate_url = 'http://%s%s' % (self.site.domain, activate_url)
-
-        self.assertEmailContains(full_activate_url)
 
     def test_unsubscribe_request_activate(self):
         """ Update a request. """
@@ -498,6 +436,8 @@ class AnonymousSubscribeTestCase(WebSubscribeTestCase, ComparingTestCase):
         subscription = getattr(r.context['form'], 'instance', None)
         self.assert_(subscription)
         self.assert_(subscription.unsubscribed)
+        self.assertEqual(subscription.name, testname2)
+        self.assertEqual(subscription.email, testemail2)
 
         dt = (datetime.now() - subscription.unsubscribe_date).seconds
         self.assertLessThan(dt, 2)
