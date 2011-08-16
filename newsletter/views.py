24f5526469d1ca2c1f160b86ae1a74414951372c
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2011-08-16 11:51:58 +0200

    Fixed some redundant imports.

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,14 +2,12 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-from datetime import datetime
-
 from django.conf import settings
 
 from django.template import RequestContext
 
-from django.shortcuts import get_object_or_404, get_list_or_404, render_to_response
-from django.http import HttpResponseRedirect, HttpResponse, Http404
+from django.shortcuts import get_object_or_404, render_to_response
+from django.http import HttpResponse, Http404
 
 from django.views.generic import list_detail, date_based 
 
