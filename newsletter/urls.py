00cc5fbb98122dd7f023ef6acce40281bd07f4e4
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-10-27 15:41:38 +0200

    Further cleanup of URL's.

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -4,29 +4,31 @@ from surlex.dj import surl
 
 
 urlpatterns = patterns('newsletter.views',
-    surl(r'^$', 'newsletter_list', name='newsletter_list'),
+    surl('^$', 'newsletter_list', name='newsletter_list'),
 
-    surl(r'^<newsletter_slug:s>/$',
+    surl('^<newsletter_slug:s>/$',
         'newsletter_detail', name='newsletter_detail'),
 
-    surl(r'^<newsletter_slug:s>/subscribe/$', 'subscribe_request',
+    surl('^<newsletter_slug:s>/subscribe/$', 'subscribe_request',
         name='newsletter_subscribe_request'),
-    surl(r'^<newsletter_slug:s>/subscribe/confirm/$', 'subscribe_request',
+    surl('^<newsletter_slug:s>/subscribe/confirm/$', 'subscribe_request',
         kwargs={'confirm': True}, name='newsletter_subscribe_confirm'),
-    surl(r'^<newsletter_slug:s>/update/$', 'update_request',
+    surl('^<newsletter_slug:s>/update/$', 'update_request',
         name='newsletter_update_request'),
-    surl(r'^<newsletter_slug:s>/unsubscribe/$', 'unsubscribe_request',
+    surl('^<newsletter_slug:s>/unsubscribe/$', 'unsubscribe_request',
         name='newsletter_unsubscribe_request'),
-    surl(r'^<newsletter_slug:s>/unsubscribe/confirm/$', 'unsubscribe_request',
+    surl('^<newsletter_slug:s>/unsubscribe/confirm/$', 'unsubscribe_request',
         kwargs={'confirm': True}, name='newsletter_unsubscribe_confirm'),
 
-    surl(r'^<newsletter_slug:s>/subscription/<email=[-_a-zA-Z0-9@\.\+~]+>/<action:s>/activate/<activation_code:s>/$',
+    surl('^<newsletter_slug:s>/subscription/<email=[-_a-zA-Z0-9@\.\+~]+>/'
+         '<action:s>/activate/<activation_code:s>/$',
         'update_subscription', name='newsletter_update_activate'),
-    surl(r'^<newsletter_slug:s>/subscription/<email=[-_a-zA-Z0-9@\.\+~]+>/<action:s>/activate/$',
+    surl('^<newsletter_slug:s>/subscription/<email=[-_a-zA-Z0-9@\.\+~]+>/'
+         '<action:s>/activate/$',
         'update_subscription', name='newsletter_update'),
 
-    surl(r'^<newsletter_slug:s>/archive/<year:Y>/<month:m>/<day:d>/<slug:s>/$',
+    surl('^<newsletter_slug:s>/archive/<year:Y>/<month:m>/<day:d>/<slug:s>/$',
         'archive_detail', name='newsletter_archive_detail'),
-    surl(r'^<newsletter_slug:s>/archive/$',
+    surl('^<newsletter_slug:s>/archive/$',
         'archive', name='newsletter_archive'),
 )
