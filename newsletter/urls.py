5e28790895f864324f74b81388636f9a05772b53
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2012-10-27 15:34:50 +0200

    Use Surlex for readable URL patterns.

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,21 +1,22 @@
 from django.conf.urls.defaults import *
 
+from surlex.dj import surl
+
 
 urlpatterns = patterns('newsletter.views',
-    # TODO: Use surlex here to simplify regexes
-    url(r'^$', 'newsletter_list', name='newsletter_list'),
+    surl(r'^$', 'newsletter_list', name='newsletter_list'),
 
-    url(r'^(?P<newsletter_slug>[-\w]+)/$','newsletter_detail', name='newsletter_detail'),
+    surl(r'^<newsletter_slug:s>/$','newsletter_detail', name='newsletter_detail'),
 
-    url(r'^(?P<newsletter_slug>[-\w]+)/subscribe/$', 'subscribe_request', name='newsletter_subscribe_request'),
-    url(r'^(?P<newsletter_slug>[-\w]+)/subscribe/confirm/$', 'subscribe_request', kwargs={'confirm':True}, name='newsletter_subscribe_confirm'),
-    url(r'^(?P<newsletter_slug>[-\w]+)/update/$', 'update_request', name='newsletter_update_request'),
-    url(r'^(?P<newsletter_slug>[-\w]+)/unsubscribe/$', 'unsubscribe_request', name='newsletter_unsubscribe_request'),
-    url(r'^(?P<newsletter_slug>[-\w]+)/unsubscribe/confirm/$', 'unsubscribe_request', kwargs={'confirm':True}, name='newsletter_unsubscribe_confirm'),
+    surl(r'^<newsletter_slug:s>/subscribe/$', 'subscribe_request', name='newsletter_subscribe_request'),
+    surl(r'^<newsletter_slug:s>/subscribe/confirm/$', 'subscribe_request', kwargs={'confirm':True}, name='newsletter_subscribe_confirm'),
+    surl(r'^<newsletter_slug:s>/update/$', 'update_request', name='newsletter_update_request'),
+    surl(r'^<newsletter_slug:s>/unsubscribe/$', 'unsubscribe_request', name='newsletter_unsubscribe_request'),
+    surl(r'^<newsletter_slug:s>/unsubscribe/confirm/$', 'unsubscribe_request', kwargs={'confirm':True}, name='newsletter_unsubscribe_confirm'),
 
-    url(r'^(?P<newsletter_slug>[-\w]+)/subscription/(?P<email>[-_a-zA-Z0-9@\.\+~]+)/(?P<action>[a-z]+)/activate/(?P<activation_code>[a-zA-Z0-9]+)/$', 'update_subscription', name='newsletter_update_activate'),
-    url(r'^(?P<newsletter_slug>[-\w]+)/subscription/(?P<email>[-_a-zA-Z0-9@\.\+~]+)/(?P<action>[a-z]+)/activate/$', 'update_subscription', name='newsletter_update'),
+    surl(r'^<newsletter_slug:s>/subscription/<email=[-_a-zA-Z0-9@\.\+~]+>/<action:s>/activate/<activation_code:s>/$', 'update_subscription', name='newsletter_update_activate'),
+    surl(r'^<newsletter_slug:s>/subscription/<email=[-_a-zA-Z0-9@\.\+~]+>/<action:s>/activate/$', 'update_subscription', name='newsletter_update'),
 
-    url(r'^(?P<newsletter_slug>[-\w]+)/archive/(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\w{1,2})/(?P<slug>[-\w]+)/$', 'archive_detail', name='newsletter_archive_detail'),
-    url(r'^(?P<newsletter_slug>[-\w]+)/archive/$', 'archive', name='newsletter_archive'),
+    surl(r'^<newsletter_slug:s>/archive/<year:Y>/<month:m>/<day:d>/<slug:s>/$', 'archive_detail', name='newsletter_archive_detail'),
+    surl(r'^<newsletter_slug:s>/archive/$', 'archive', name='newsletter_archive'),
 )
