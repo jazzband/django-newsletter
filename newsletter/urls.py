d85543867191a579f47e997949bca2f3a95a6d0a
Jannis <jvajen@gmail.com>
2013-11-07 19:01:45 +0100

    Remove surlex dependency.
    See #97; surlex does not support Django 1.6 and seems not to be
    actively maintained.

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -1,6 +1,4 @@
-from django.conf.urls import patterns
-
-from surlex.dj import surl
+from django.conf.urls import patterns, url
 
 from .views import (
     NewsletterListView, NewsletterDetailView,
@@ -11,74 +9,81 @@ from .views import (
 
 urlpatterns = patterns('newsletter.views',
     # Newsletter list and detail view
-    surl('^$', NewsletterListView.as_view(), name='newsletter_list'),
-    surl(
-        '^<newsletter_slug:s>/$',
+    url('^$', NewsletterListView.as_view(), name='newsletter_list'),
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/$',
         NewsletterDetailView.as_view(), name='newsletter_detail'
     ),
 
     # Action request views
-    surl(
-        '^<newsletter_slug:s>/subscribe/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/subscribe/$',
         SubscribeRequestView.as_view(),
         name='newsletter_subscribe_request'
     ),
-    surl(
-        '^<newsletter_slug:s>/subscribe/confirm/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/subscribe/confirm/$',
         SubscribeRequestView.as_view(confirm=True),
         name='newsletter_subscribe_confirm'
     ),
-    surl(
-        '^<newsletter_slug:s>/update/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/update/$',
         UpdateRequestView.as_view(),
         name='newsletter_update_request'
     ),
-    surl(
-        '^<newsletter_slug:s>/unsubscribe/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/unsubscribe/$',
         UnsubscribeRequestView.as_view(),
         name='newsletter_unsubscribe_request'
     ),
-    surl(
-        '^<newsletter_slug:s>/unsubscribe/confirm/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/unsubscribe/confirm/$',
         UnsubscribeRequestView.as_view(confirm=True),
         name='newsletter_unsubscribe_confirm'
     ),
 
     # Activation email sent view
-    surl('^<newsletter_slug:s>/<action=subscribe|update|unsubscribe>/'
-         'email-sent/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/'
+        '(?P<action>(subscribe|update|unsubscribe))/email-sent/$',
         ActionTemplateView.as_view(
             template_name='newsletter/subscription_%(action)s_email_sent.html'
         ),
         name='newsletter_activation_email_sent'),
 
     # Action confirmation views
-    surl(
-        '^<newsletter_slug:s>/subscription/<email=[-_a-zA-Z0-9@\.\+~]+>/'
-        '<action=subscribe|update|unsubscribe>/activate/<activation_code:s>/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/subscription/'
+        '(?P<email>[-_a-zA-Z0-9@\.\+~]+)/'
+        '(?P<action>(subscribe|update|unsubscribe))/'
+        'activate/(?P<activation_code>[\w-]+)/$',
         UpdateSubscriptionViev.as_view(), name='newsletter_update_activate'
     ),
-    surl(
-        '^<newsletter_slug:s>/subscription/<email=[-_a-zA-Z0-9@\.\+~]+>/'
-        '<action=subscribe|update|unsubscribe>/activate/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/subscription/'
+        '(?P<email>[-_a-zA-Z0-9@\.\+~]+)/'
+        '(?P<action>(subscribe|update|unsubscribe))/activate/$',
         UpdateSubscriptionViev.as_view(), name='newsletter_update'
     ),
 
     # Action activation completed view
-    surl('^<newsletter_slug:s>/<action=subscribe|update|unsubscribe>/'
-        'activation-completed/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/'
+        '(?P<action>(subscribe|update|unsubscribe))/activation-completed/$',
         ActionTemplateView.as_view(
             template_name='newsletter/subscription_%(action)s_activated.html'
         ),
         name='newsletter_action_activated'),
 
     # Archive views
-    surl(
-        '^<newsletter_slug:s>/archive/<year:Y>/<month:m>/<day:d>/<slug:s>/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/archive/'
+        '(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/'
+        '(?P<slug>[\w-]+)/$',
         SubmissionArchiveDetailView.as_view(), name='newsletter_archive_detail'
     ),
-    surl(
-        '^<newsletter_slug:s>/archive/$',
+    url(
+        '^(?P<newsletter_slug>[\w-]+)/archive/$',
         SubmissionArchiveIndexView.as_view(), name='newsletter_archive'
     ),
 )
