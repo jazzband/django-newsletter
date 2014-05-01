78caba0962c5f5cf51502fb84d49c9b2bd2f8412
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2014-05-01 16:21:07 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ except:
 
 setup(
     name='django-newsletter',
-    version="0.5.1",
+    version="0.5.2",
     description=(
         'Django app for managing multiple mass-mailing lists with both '
         'plaintext as well as HTML templates (and pluggable WYSIWYG editors '
