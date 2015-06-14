85cb6c429786ddd3bd047efa8c93fc168a25168e
fpghost <leehodg@gmail.com>
2015-06-14 19:35:57 +0700

    Change queryset to get_queryset for Django > 1.6

--- newsletter/admin_utils.py
+++ newsletter/admin_utils.py
@@ -12,7 +12,7 @@ class ExtendibleModelAdminMixin(object):
             opts = self.model._meta
 
             try:
-                obj = self.queryset(request).get(pk=unquote(object_id))
+                obj = self.get_queryset(request).get(pk=unquote(object_id))
             except self.model.DoesNotExist:
                 # Don't raise Http404 just yet, because we haven't checked
                 # permissions yet. We don't want an unauthenticated user to
