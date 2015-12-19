306c9794b06360bd87fd7cea1cabcf82d1daa4e6
Sergei Maertens <sergei@maykinmedia.nl>
2015-12-19 19:43:26 +0100

    Prefer new test runner above old one

--- runtests.py
+++ runtests.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python# Run tests for application
+#!/usr/bin/env python  # Run tests for application
 # Source: https://stackoverflow.com/questions/3841725/how-to-launch-tests-for-django-reusable-app
 
 import sys
@@ -15,13 +15,13 @@ def runtests():
         django.setup()
 
     try:
-        # Django <= 1.8
-        from django.test.simple import DjangoTestSuiteRunner
-        test_runner = DjangoTestSuiteRunner(verbosity=1)
-    except ImportError:
-        # Django >= 1.8
+        # Django >= 1.6
         from django.test.runner import DiscoverRunner
         test_runner = DiscoverRunner(verbosity=1)
+    except ImportError:
+        # Django < 1.6
+        from django.test.simple import DjangoTestSuiteRunner
+        test_runner = DjangoTestSuiteRunner(verbosity=1)
 
     failures = test_runner.run_tests(['newsletter'])
     sys.exit(failures)
