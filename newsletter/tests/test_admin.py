db75b25d9ec4e0ffecae3ee2d56b128853af9bab
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-01-04 12:37:33 +0100

    Regression test for imports with duplicates.

--- newsletter/tests/test_admin.py
+++ newsletter/tests/test_admin.py
@@ -24,21 +24,20 @@ class AdminTestCase(TestCase):
             slug='test-newsletter', visible=True, email='test@test.com',
         )
 
-    def admin_import_subscribers(self, source_file):
+    def admin_import_subscribers(self, source_file, ignore_errors=''):
         """
         Import process of a CSV/LDIF/VCARD file containing subscription
         addresses from the admin site.
         """
         import_url = reverse('admin:newsletter_subscription_import')
-        response = self.client.get(import_url)
-        self.assertContains(response, "<h1>Import addresses</h1>")
 
         with open(source_file, 'rb') as fh:
             response = self.client.post(import_url, {
                 'newsletter': self.newsletter.pk,
                 'address_file': fh,
-                'ignore_errors': '',
+                'ignore_errors': ignore_errors,
             }, follow=True)
+
         self.assertContains(response, "<h1>Confirm import</h1>")
 
         import_confirm_url = reverse(
@@ -53,6 +52,13 @@ class AdminTestCase(TestCase):
         )
         self.assertEqual(self.newsletter.subscription_set.count(), 2)
 
+    def test_admin_import_form(self):
+        """ Test Import form. """
+
+        import_url = reverse('admin:newsletter_subscription_import')
+        response = self.client.get(import_url)
+        self.assertContains(response, "<h1>Import addresses</h1>")
+
     def test_admin_import_subscribers_csv(self):
         source_file = os.path.join(test_files_dir, 'addresses.csv')
         self.admin_import_subscribers(source_file)
@@ -64,3 +70,9 @@ class AdminTestCase(TestCase):
     def test_admin_import_subscribers_vcf(self):
         source_file = os.path.join(test_files_dir, 'addresses.vcf')
         self.admin_import_subscribers(source_file)
+
+    def test_admin_import_subscribers_duplicates(self):
+        """ Test importing a file with duplicate addresses. """
+        source_file = os.path.join(test_files_dir, 'addresses_duplicates.csv')
+
+        self.admin_import_subscribers(source_file, ignore_errors='true')
