0d63a90872b25fd7ff0d543eb4251e4196d26fcf
Ganuta Aleksey <ziemenz@eventmap.io>
2016-01-19 20:46:05 +0400

    Fixed get_version ImportError if Django not installed

--- newsletter/__init__.py
+++ newsletter/__init__.py
@@ -1,4 +1,7 @@
-from django.utils.version import get_version
+try:
+    from django.utils.version import get_version
+except ImportError:
+    from .get_version import get_version
 
 VERSION = (0, 6, 0, 'alpha', 0)
 
