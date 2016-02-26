3235a3f7718021a46c1a9fa2f1a48251fa0b670e
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-02-26 16:56:31 +0100

    Renumerate article sortorder during migration.

--- newsletter/migrations/0003_auto_20160226_1518.py
+++ newsletter/migrations/0003_auto_20160226_1518.py
@@ -1,7 +1,22 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import logging
+logger = logging.getLogger(__name__)
 
 from django.db import migrations, models
+
+
+def renumerate_article_sortorder(apps, schema_editor):
+    """ Renumerate articles for consistent and guaranteed unique sortorder. """
+
+    Message = apps.get_model('newsletter', 'Message')
+
+    for message in Message.objects.all():
+        for index, article in enumerate(message.articles.all()):
+            # We're using the fact that articles are ordered by default
+
+            article.sortorder = (index + 1) * 10
+            article.save()
 
 
 class Migration(migrations.Migration):
@@ -16,6 +31,7 @@ class Migration(migrations.Migration):
             name='sortorder',
             field=models.PositiveIntegerField(help_text='Sort order determines the order in which articles are concatenated in a post.', verbose_name='sort order', blank=True),
         ),
+        migrations.RunPython(renumerate_article_sortorder),
         migrations.AlterUniqueTogether(
             name='article',
             unique_together=set([('post', 'sortorder')]),
