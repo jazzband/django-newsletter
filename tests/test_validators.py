70aecc1e58f6c476dd064510b78343e2dfa7fd21
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2016-05-21 17:01:56 +0200

    Coverage for validators.

---
+++ tests/test_validators.py
@@ -0,0 +1,24 @@
+from django.test import TestCase
+from django.forms.utils import ValidationError
+from django.contrib.auth import get_user_model
+
+from newsletter.validators import validate_email_nouser
+
+
+class ValidatorTestCase(TestCase):
+    """ Test case for validators. """
+
+    def test_validate_email_nouser_noerror(self):
+        """ Test validate_email_nouser where no error is raised. """
+        validate_email_nouser('test@nowhere.com')
+
+    def test_validate_email_nouser_error(self):
+        """ Test validate_email_nouser where error is raised. """
+        User = get_user_model()
+        password = User.objects.make_random_password()
+        user = User.objects.create_user(
+            'john', 'lennon@thebeatles.com', password)
+        user.save()
+
+        with self.assertRaises(ValidationError):
+            validate_email_nouser(user.email)
