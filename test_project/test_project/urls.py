97ae213302be6c9b31726941d62f42ec48886d36
Peter (dkdndes) <dkdndes@gmail.com>
2017-06-19 19:29:29 +0200

    2to3, isort, autopep8

--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^newsletter/', include('newsletter.urls')),
