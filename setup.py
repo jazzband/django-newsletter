c94b1b2330493cd8f2d3f42a20ce422c8ef6da42
ed@sharpertool.com <ed@sharpertool.com>
2019-02-08 17:07:21 -0700

    Update version to 0.8b2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ except:
 
 setup(
     name='django-newsletter',
-    version="0.8b1",
+    version="0.8b2",
     description=(
         'Django app for managing multiple mass-mailing lists with both '
         'plaintext as well as HTML templates (and pluggable WYSIWYG editors '
