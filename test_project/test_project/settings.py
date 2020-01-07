ac9d07ac72648e6d91341562b6aa4514724a42fd
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2020-01-07 15:49:25 +0100

    Remove legacy MIDDLEWARE_CLASSES.

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -31,7 +31,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
-MIDDLEWARE_CLASSES = MIDDLEWARE  # Django < 1.10
 
 ROOT_URLCONF = 'test_project.urls'
 
