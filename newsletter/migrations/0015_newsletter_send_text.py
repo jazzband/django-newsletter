6262f1a7e69aabe1c55dc78b0baeabc64d106ed3
Maxime Barbier <maxime@maxime-barbier.fr>
2026-03-25 11:47:48 +0100

    update migratiion due to merge with master

--- newsletter/migrations/0015_newsletter_send_text.py
+++ newsletter/migrations/0015_newsletter_send_text.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("newsletter", "0013_submission_site"),
+        ("newsletter", "0014_article_image_below_text"),
     ]
 
     operations = [
